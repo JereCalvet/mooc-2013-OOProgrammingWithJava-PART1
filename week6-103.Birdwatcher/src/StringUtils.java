5cd4c63335ad612ef000542bbcfb53da4e76bba3
Jereaaa <jere_calvet@hotmail.com>
2020-02-08 23:49:14 -0300

    1+

---
+++ week6-103.Birdwatcher/src/StringUtils.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author Jere
+ */
+public class StringUtils {
+   public static boolean EqualsIgnoreCase(String command, String input){
+        if (command.isEmpty() || input.isEmpty()){
+            return false;
+        } 
+        return command.toUpperCase().equals(input.toUpperCase());
+    }  
+}
