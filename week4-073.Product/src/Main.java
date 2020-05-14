faab0ff855bbad8f229f56c2239cf9fd95f517d5
Jereaaa <jere_calvet@hotmail.com>
2020-05-14 12:49:28 -0300

    Estructura de clases
     Clases: *formato
                *contructores
                *metodos
                *Objetos como variables

--- week4-073.Product/src/Main.java
+++ week4-073.Product/src/Main.java
@@ -1,3 +1,10 @@
+/*
+Tema: 
+    Clases: *formato
+            *contructores
+            *metodos
+            *Objetos como variables
+*/
 public class Main {
     public static void main(String[] args) {
         // You can test your new class here, try e.g.:
